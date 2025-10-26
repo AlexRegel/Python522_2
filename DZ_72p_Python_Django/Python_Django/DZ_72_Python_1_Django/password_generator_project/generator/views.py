59b767ec355ba05a32d03bf764dcdf5b0c1bc9d2
AlexRegel <alexanfedich888@mail.ru>
2025-10-27 01:34:24 +0300

    DZ_72_Python_1_Django

---
+++ DZ_72p_Python_Django/Python_Django/DZ_72_Python_1_Django/password_generator_project/generator/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+def home(request):
+    lst = list(range(6, 15))
+    print(lst)
+    return render(request, "generator/home.html",
+                  {"lst": lst})
+
+
+def password(request):
+    char = [chr(i) for i in range(97, 123)]
+
+    if request.GET.get('uppercase'):
+        char.extend([chr(i) for i in range(65, 91)])
+
+    if request.GET.get('number'):
+        char.extend([chr(i) for i in range(48, 57)])
+
+    if request.GET.get('special'):
+        char.extend([chr(i) for i in range(33, 47)])
+
+    print(char)
+    length = int(request.GET.get('length'))
+    psw = ""
+    for i in range(length):
+        psw += random.choice(char)
+    return render(request,
+                  "generator/password.html",
+                  {'password': psw})
+
+
+def info(request):
+    ptext = """Для генерации пароля выберите количество символов
+    в выпадающем списке. Затем отметьте галочки,
+    соответствующие изменению сложности пароля:
+    Верхний регистр, цифры и/или спецсимволы. 
+    После установки опций кликните по клавише 
+    "Сгенерировать пароль". """
+
+    return render(request,
+                  "generator/info.html",
+                  {'info': ptext})
--- main.py
+++ main.py
@@ -6815,20 +6815,20 @@ from jinja2 import Template
 # print(msg)
 
 
-html = """
-{% macro input_func(name, value='', type='text', size=20) %}
-    <input type="{{ type }}" name="{{ name }}" value="{{ value }}" size="{{ size }}">
-{% endmacro %}
-
-<p>{{ input_func('username') }}</p>
-<p>{{ input_func('email') }}</p>
-<p>{{ input_func('password', type='password') }}</p>
-"""
-
-tm = Template(html)
-msg = tm.render()
-
-print(msg)
+# html = """
+# {% macro input_func(name, value='', type='text', size=20) %}
+#     <input type="{{ type }}" name="{{ name }}" value="{{ value }}" size="{{ size }}">
+# {% endmacro %}
+#
+# <p>{{ input_func('username') }}</p>
+# <p>{{ input_func('email') }}</p>
+# <p>{{ input_func('password', type='password') }}</p>
+# """
+#
+# tm = Template(html)
+# msg = tm.render()
+#
+# print(msg)
 
 # ---------------------------------------------------------
 #  lesson 72
