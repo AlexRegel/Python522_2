5fcf2b3527e6f4091fb1752a0e234059c7ba6144
AlexRegel <alexanfedich888@mail.ru>
2025-11-26 18:03:35 +0300

    lesson_81_Python_10_1

--- fourth/devsearch/projects/views.py
+++ fourth/devsearch/projects/views.py
@@ -2,11 +2,43 @@ from django.shortcuts import render, redirect
 from .models import Project
 from .forms import ProjectForm
 from django.contrib.auth.decorators import login_required
+from .utils import search_projects
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def projects(request):
-    pr = Project.objects.all()
-    context = {'projects': pr}
+    pr, search_query = search_projects(request)
+
+    page = request.GET.get('page')
+    result = 2
+    paginator = Paginator(pr, result)
+
+    try:
+        pr = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        pr = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        pr = paginator.page(page)
+
+    left_index = int(page) - 4
+
+    if left_index < 1:
+        left_index = 1
+
+    right_index = int(page) + 5
+
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+
+    context = {'projects': pr,
+               'search_query': search_query,
+               'paginator': paginator,
+               'custom_range': custom_range
+               }
     return render(request, 'projects/projects.html', context)
 
 
